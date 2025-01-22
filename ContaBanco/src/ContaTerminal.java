bae66496e8206376854b7721ee40ab8f7f588cd0
Gabriela Rodrigues de Souza <gabriela.souza.investimentos@gmail.com>
2025-01-22 19:52:32 -0300

    Criando código para lógica do Contabanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        //TO DO: 
+                 //Conhecer e importar a classe scanner 
+                 //Exibir as mensagens para o nosso usuário
+                 //Obter pelo scanner os valores digitados no terminal 
+                 //Exibir a mensagem conta criada
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome_cliente = entrada.nextLine();
+
+        System.out.println("Digite a sua agência: ");
+        String agencia_cliente = entrada.nextLine();
+
+        System.out.println("Digite o número da agência: ");
+        int numero_agencia_cliente = entrada.nextInt();
+
+        System.out.println("Digite o seu saldo: ");
+        double saldo_cliente = entrada.nextDouble();
+
+        System.out.println("Olá " +nome_cliente+", obrigado por criar uma conta em nosso banco, sua agência é " 
+        +agencia_cliente+ ", conta " +numero_agencia_cliente+ " e seu saldo " +saldo_cliente+ " já está disponível para saque");
     }
 }
